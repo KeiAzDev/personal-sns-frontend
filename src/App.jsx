e8e0237eb580238b7c1e811b48af325883b8724f
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-14 15:58:56 -0800

    added home and topbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { useState } from 'react'
+import Home from './pages/home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        
-      </div>
-    </>
-  )
+  return <Home />
 }
 
 export default App
