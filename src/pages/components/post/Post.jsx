e3e1ef0de811e45121c07473c8c3c6f2b81e6d39
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-15 13:44:52 -0800

    post component styled

--- src/pages/components/post/Post.jsx
+++ src/pages/components/post/Post.jsx
@@ -7,12 +7,14 @@ export default function Post() {
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
-          <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
-          <span className="postUsername">Shin code</span>
-          <span className="postDate">5分前</span>
-        </div>
-        <div className="postRight">
-          <MoreVert />
+          <div className="postTopLeft">
+            <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
+            <span className="postUsername">Shin code</span>
+            <span className="postDate">5分前</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
         <div className="postCenter">
           <span className="postText">SNSを自作中</span>
@@ -20,7 +22,7 @@ export default function Post() {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="./assets/heart.png" alt="" className="lileIcon" />
+            <img src="./assets/heart.png" alt="" className="likeIcon" />
             <span className="postLikeCounter">
               ５人がいいね押しました
             </span>
