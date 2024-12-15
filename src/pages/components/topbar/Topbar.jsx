ddc9ca1ef35ec4408e3409b77066dff900707a5f
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-14 20:44:11 -0800

    done topbar styling

--- src/pages/components/topbar/Topbar.jsx
+++ src/pages/components/topbar/Topbar.jsx
@@ -10,21 +10,23 @@ export default function Topbar() {
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
-          <Search className='searchIcon' style={{fontSize: 20}} />
+          <Search className='searchIcon' style={{ fontSize: 20 }} />
           <input type="text" className='searchInput' placeholder='探し物はなんですか' />
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">
-          <Chat />
-          <span className="topbarIconBadge">1</span>
-        </div>
-        <div className="topbarIconItem">
-          <Notifications />
-          <span className="topbarIconBadge">2</span>
+        <div className="topbarItemIcons">
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <img src="/assets/person/1.jpeg" alt="" className='topbarImg' />
         </div>
       </div>
-      <img src="/assets/person/1.jpeg" alt="" className='topbarImg' />
     </div>
   )
 }
