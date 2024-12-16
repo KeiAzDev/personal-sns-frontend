b4ea211249fb5701455e68572043f74dcf8c2392
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-16 11:09:28 -0800

    added switch to home and profile rightbar

--- src/pages/components/rightbar/Rightbar.jsx
+++ src/pages/components/rightbar/Rightbar.jsx
@@ -3,20 +3,21 @@ import "./Rightbar.css";
 import { Users } from '../../../dummyData';
 import Online from '../online/Online';
 
-export default function Rightbar() {
-  return (
-    <div className='rightbar'>
-      <div className="rightbarWrapper">
+export default function Rightbar({ profile }) {
+
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className="eventContainer">
           <img src="assets/star.png" alt="" className="starImg" />
           <span className="evnetText">
             <b>フォロワー限定</b>イベント開催中
           </span>
         </div>
-        <img src="assets/ad.jpeg" alt="" className="eventImg"/>
+        <img src="assets/ad.jpeg" alt="" className="eventImg" />
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
-          {Users.map((user) => (<Online user={user} key={user.id}/>))}
+          {Users.map((user) => (<Online user={user} key={user.id} />))}
         </ul>
         <p className="promotionTitle">プロモーション広告</p>
         <img src="assets/promotion/promotion1.jpeg" alt="" className="rightbarPromotionImg" />
@@ -25,6 +26,22 @@ export default function Rightbar() {
         <p className="promotionName">カーショップ</p>
         <img src="assets/promotion/promotion3.jpeg" alt="" className="rightbarPromotionImg" />
         <p className="promotionName">hogehoge株式会社</p>
+      </>
+    )
+  }
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        profile rightbar
+      </>
+    )
+  }
+
+  return (
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
       </div>
     </div>
   )
