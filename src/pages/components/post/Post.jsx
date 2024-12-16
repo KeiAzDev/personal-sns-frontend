2a1f0ea1d4efd5dc00cb2436bd4cd573604cff08
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-15 17:30:53 -0800

    added dummydate and getting connected

--- src/pages/components/post/Post.jsx
+++ src/pages/components/post/Post.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Post.css";
 import { MoreVert } from "@mui/icons-material";
 
-export default function Post() {
+export default function Post({post}) {
   return (
     <div className="post">
       <div className="postWrapper">
@@ -10,25 +10,25 @@ export default function Post() {
           <div className="postTopLeft">
             <img src="./assets/person/1.jpeg" alt="" className="postProfileImg" />
             <span className="postUsername">Shin code</span>
-            <span className="postDate">5分前</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">SNSを自作中</span>
-          <img src="./assets/post/1.jpeg" alt="" className="postImg" />
+          <span className="postText">{post.desc}</span>
+          <img src={post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img src="./assets/heart.png" alt="" className="likeIcon" />
             <span className="postLikeCounter">
-              ５人がいいね押しました
+              {post.like}人がいいね押しました
             </span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">4:コメント</span>
+            <span className="postCommentText">{post.comment}:コメント</span>
           </div>
         </div>
       </div>
