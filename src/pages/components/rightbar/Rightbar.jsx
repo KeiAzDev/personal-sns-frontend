1a0a347881c60d1b84779c194db6678abe5d511a
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-15 16:17:44 -0800

    added rightbar component

--- src/pages/components/rightbar/Rightbar.jsx
+++ src/pages/components/rightbar/Rightbar.jsx
@@ -4,7 +4,25 @@ import "./Rightbar.css";
 export default function Rightbar() {
   return (
     <div className='rightbar'>
-      right
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="assets/star.png" alt="" className="starImg" />
+          <span className="evnetText">
+            <b>フォロワー限定</b>イベント開催中
+          </span>
+        </div>
+        <img src="assets/ad.jpeg" alt="" />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightProfileImgContainer">
+              <img src="assets/person/1.jpeg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Shin Code</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
